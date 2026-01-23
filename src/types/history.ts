aec02f5bc78cf4656c71488e186f06be8b050fa6
권희정 <heejeongkwon@gwonhuijeong-ui-MacBookPro.local>
2026-01-23 19:45:38 +0900

    add: 응시 내역 조회용 타입 정의 추가

---
+++ src/types/history.ts
@@ -0,0 +1,13 @@
+export type HistoryItem = {
+  exam_id: number
+  history_id: number
+  exam_title: string
+  subject_name: string
+  nickname: string
+  course_name: string
+  cohort_number: number
+  cheating_count: number
+  score: number
+  started_at: string
+  finished_at: string
+}
