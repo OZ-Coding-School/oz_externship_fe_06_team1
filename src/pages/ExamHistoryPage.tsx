3a852ab908eedd11147236de6a595a46ab796764
권희정 <heejeongkwon@gwonhuijeong-ui-MacBookPro.local>
2026-01-28 15:51:41 +0900

    fix: AdminLayout  경로 및 title 수정

--- src/pages/ExamHistoryPage.tsx
+++ src/pages/ExamHistoryPage.tsx
@@ -3,7 +3,7 @@ import HistoryList from '@/components/table/HistoryList'
 
 export default function ExamHistoryPage() {
   return (
-    <AdminContainer title="응시 내역 관리">
+    <AdminContainer title="쪽지시험 응시 내역 조회">
       <HistoryList />
     </AdminContainer>
   )
